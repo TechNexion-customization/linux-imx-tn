90384e5d1f3c253458b6f32a960f33648543efa7
Richard Hu <richard.hu@technexion.com>
2022-07-06 11:40:35 +0800

    Vizionpanel: test read/write remote side(DES) EEPROM

--- drivers/gpu/drm/bridge/ds90ub94x.c
+++ drivers/gpu/drm/bridge/ds90ub94x.c
@@ -34,10 +34,10 @@ static void ds90ub94x_write_reg(struct i2c_client *client, u8 reg, u8 data)
 
 static void ds90ub94x_display_setting(void)
 {
-    printk("------------>%s, %s\r\n", __FILE__, __FUNCTION__);
+        printk("------------>%s, %s\r\n", __FILE__, __FUNCTION__);
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x01, 0x0f); /* Reset DSI/DIGITLE */
 	usleep_range(5000, 6000);	/* time cannot be too short */
-    ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x03, 0xBA); /* Enable FPD-Link I2C pass through */
+        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x03, 0xBA); /* Enable FPD-Link I2C pass through */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x1E, 0x01); /* Select FPD-Link III Port 0 */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x5B, 0x21); /* FPD3_TX_MODE=single, Reset PLL */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x4F, 0x8C); /* DSI Continuous Clock Mode,DSI 4 lanes */
@@ -49,10 +49,23 @@ static void ds90ub94x_display_setting(void)
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x01, 0x01); /* ds90ub948 reset */
 	usleep_range(10000, 11000);	/* time cannot be too short */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x49, 0x62); /* Set FPD_TX_MODE, MAPSEL=1(SPWG), Single OLDI output */
-    /* Set GPIO via local I2C */
-    ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x01); /* Select FPD-Link III Port 0, GPIOx instead of D_GPIOx */
+        /* Set GPIO via local I2C */
+        //ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x01); /* Select FPD-Link III Port 0, GPIOx instead of D_GPIOx */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x34, 0x02); /* Select FPD-Link III Port 0, GPIOx instead of D_GPIOx */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1D, 0x19); /* GPIO0, MIPI_BL_EN */
 	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x1E, 0x99); /* GPIO1, MIPI_VDDEN; GPIO2, MIPI_BL_PWM */
+
+        /* Configure remote devcie ID in local SER */
+        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x7, 0xac); /* SlaveID_0: EEPROM */
+        ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x8, 0xac); /* SlaveAlias_0: EEPROM */
+
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x8, 0xac); /* SlaveID_0: EEPROM */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0x10, 0xac); /* SlaveAlias_0: EEPROM */
+	#if 0
+        /* Configure remote interrupt for touch panel */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub941_i2c, 0x30, 0x1); /* REM_INTB_CTRL: port 0 remote interrupt */
+	ds90ub94x_write_reg(g_ds90ub94x->ds90ub948_i2c, 0xC6, 0x21); /* INTB: enable INTB_IN on remote DES */
+	#endif
 }
 
 static int ds90ub94x_probe(struct i2c_client *client, const struct i2c_device_id *id)
